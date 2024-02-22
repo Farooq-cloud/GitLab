cd5c9462cd5f4d7f0b1b1d59f612f27b7318ad4e
Farooq-cloud <83739025+Farooq-cloud@users.noreply.github.com>
2024-02-22 17:20:10 +0530

    New gitlab files added
    New gitlab files added

---
+++ surge.sh
@@ -0,0 +1,10 @@
+* What is surge.sh
+
+  * Cloud platform for serverless deployments.
+  * Easy to use and configure.
+  * Simple deployments process.
+  * Ideal for static websites.
+
+# npm install --global surge
+# surge
+ Give username and password login enter and enter surge.sh with somename will appear and we can access the website with that name.
